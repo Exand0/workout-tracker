83e5fa388ee9e132e0d4ded28b88ffa96f2411d2
sasha <ku3menko.sasha@gmail.com>
2020-06-30 21:54:38 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import View from "./View.jsx";
+
+const App = () => {
+    return <View></View>;
+};
+// ReactDOM.
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ src/View.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class View extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div className="view">Hello!</div>;
+    }
+}
+
+export default View;
